64338894bfc57819db28055817a4b4451eb6c6ed
Hosam <hosamyoussef303@gmail.com>
2024-03-31 13:51:57 +0200

    Time: 17 ms (20.41%), Space: 44.2 MB (58.30%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+          Map<Character, Integer> count = new HashMap<>();
+        
+        for (char x : s.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+        
+      
+        for (char x : t.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) - 1);
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+
+        
+    }
+}
