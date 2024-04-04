6465035d4dc8c2544192d92774e4bee4ff7978ec
Hosam <hosamyoussef303@gmail.com>
2024-04-04 21:06:50 +0200

    Time: 901 ms (31.39%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,6 @@
-select FirstName, LastName, City, State
-from Person left join Address
-on Person.PersonId = Address.PersonId
-;
+SELECT p.firstName,
+       p.lastName,
+       a.city,
+       a.state 
+from Person p left outer join Address a 
+on p.personId=a.personId;
