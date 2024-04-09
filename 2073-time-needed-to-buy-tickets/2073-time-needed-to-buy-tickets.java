c74efeeb61dc08a07d79af42df359ceca0333194
Hosam <hosamyoussef303@gmail.com>
2024-04-09 04:47:56 +0200

    Time: 2 ms (70.45%), Space: 41 MB (67.71%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,27 +1,19 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int n = tickets.length;
         int time = 0;
+        while(tickets[k] > 0){
+            for(int i = 0; i < tickets.length;i++){
+                if(tickets[i] ==0 )
+                    continue;
 
-        // If person k only needs one ticket, return the time to buy it
-        if (tickets[k] == 1)
-            return k + 1;
-
-        // Continue buying tickets until person k buys all their tickets
-        while (tickets[k] > 0) {
-            for (int i = 0; i < n; i++) {
-                // Buy a ticket at index 'i' if available
-                if (tickets[i] != 0) {
-                    tickets[i]--;
-                    time++;
-                }
+                if(tickets[k] ==0 )
+                    return time; 
                 
-                // If person k bought all their rickets, return the time
-                if (tickets[k] == 0)
-                    return time;
+                tickets[i]--;
+                time++;
             }
+            
         }
-        
         return time;
     }
 }
